8d09fdd6150fa2aaced5e1f7315e7f20ae1ab725
Leo Deng <myst.dg@gmail.com>
2014-10-28 22:12:26 +0800

    hexo theme

---
+++ source/js/script.js
@@ -0,0 +1,28 @@
+var toggle = document.getElementById('toggle')
+var overlay = document.getElementById('overlay')
+var toggleR = document.getElementById('toggle-responsive')
+var main = document.getElementById('main')
+
+toggleR.addEventListener('click', function(e) {
+  if(document.body.className !== 'menu-open-responsive') {
+    document.body.className = 'menu-open-responsive'
+  } else {
+    document.body.className = ''
+  }
+}, false)
+
+toggle.addEventListener('click', function(e) {
+  document.body.className = 'menu-open'
+  toggle.className = 'hidden transparent'
+})
+
+overlay.addEventListener('click', function(e) {
+  document.body.className = ''
+  toggle.className = 'transparent'
+})
+
+main.addEventListener('transitionend', function(e) {
+  if(document.body.className !== 'menu-open') {
+    toggle.className = ''
+  }
+}, false)
