4159ef4bbd11bb069258af8dd4bd7ebf390f61a4
Leo Deng <myst.dg@gmail.com>
2014-11-06 20:25:46 +0800

    enclose global

--- source/js/script.js
+++ source/js/script.js
@@ -1,28 +1,30 @@
-var toggle = document.getElementById('toggle')
-var overlay = document.getElementById('overlay')
-var toggleR = document.getElementById('toggle-responsive')
-var main = document.getElementById('main')
+void function(window, document, undefined) {
+  var toggle = document.getElementById('toggle')
+  var overlay = document.getElementById('overlay')
+  var toggleR = document.getElementById('toggle-responsive')
+  var main = document.getElementById('main')
 
-toggleR.addEventListener('click', function(e) {
-  if(document.body.className !== 'menu-open-responsive') {
-    document.body.className = 'menu-open-responsive'
-  } else {
+  toggleR.addEventListener('click', function(e) {
+    if(document.body.className !== 'menu-open-responsive') {
+      document.body.className = 'menu-open-responsive'
+    } else {
+      document.body.className = ''
+    }
+  }, false)
+
+  toggle.addEventListener('click', function(e) {
+    document.body.className = 'menu-open'
+    toggle.className = 'hidden transparent'
+  })
+
+  overlay.addEventListener('click', function(e) {
     document.body.className = ''
-  }
-}, false)
+    toggle.className = 'transparent'
+  })
 
-toggle.addEventListener('click', function(e) {
-  document.body.className = 'menu-open'
-  toggle.className = 'hidden transparent'
-})
-
-overlay.addEventListener('click', function(e) {
-  document.body.className = ''
-  toggle.className = 'transparent'
-})
-
-main.addEventListener('transitionend', function(e) {
-  if(document.body.className !== 'menu-open') {
-    toggle.className = ''
-  }
-}, false)
+  main.addEventListener('transitionend', function(e) {
+    if(document.body.className !== 'menu-open') {
+      toggle.className = ''
+    }
+  }, false)
+}(window, document)
