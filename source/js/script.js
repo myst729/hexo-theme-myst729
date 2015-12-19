77a2b0295e8b462b3934a70856ac5353265fa8e3
Leo Deng <myst.dg@gmail.com>
2015-12-19 18:03:52 +0800

    fix responsive layout

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,25 @@
+/* jshint asi:true */
+'use stricy'
+
 void function(window, document, undefined) {
+  var body = document.body
   var toggle = document.getElementById('toggle')
   var overlay = document.getElementById('overlay')
   var toggleR = document.getElementById('toggle-responsive')
 
   toggleR.addEventListener('click', function(e) {
-    if(document.body.className !== 'menu-open-responsive') {
-      document.body.className = 'menu-open-responsive'
+    if (body.className !== 'menu-open-responsive') {
+      body.className = 'menu-open-responsive'
     } else {
-      document.body.className = ''
+      body.className = ''
     }
   }, false)
 
   toggle.addEventListener('click', function(e) {
-    document.body.className = 'menu-open'
+    body.className = 'menu-open'
   }, false)
 
   overlay.addEventListener('click', function(e) {
-    document.body.className = ''
+    body.className = ''
   }, false)
 }(window, document)
